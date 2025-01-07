9922e1a909293bfb67549a02cb674a031b9c723b
Louie <eliasdandouch@gmail.com>
2025-01-06 16:47:09 -0800

    able to insert machines & machine_settings

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,27 @@
 from flask import Flask, request, jsonify
-from middleware import get_user_settings, get_machines
+from middleware import get_user_settings, get_machines, add_machines, add_machine_settings
 
 def create_app():
     app = Flask(__name__)
-    @app.route("/")
+    @app.route("/settings", methods=['GET'], endpoint='get_machine_settings_middleware_endpoint')
     @get_user_settings
-    def settings():
+    def get_settings():
         middleware_data = request.middleware_data
         return jsonify(middleware_data), 200 if middleware_data["status"] == "success" else 500
+    @app.route("/settings", methods=['POST'], endpoint='add_settings_middleware_endpoint')
+    @add_machine_settings
+    def add_settings():
+        middleware_data = request.middleware_data
+        return jsonify(middleware_data), 200 if middleware_data["status"] == "success" else 500
+    
     @app.route('/machines', methods=['GET'], endpoint='machines_middleware_endpoint')
     @get_machines
     def machines_middleware():
+        middleware_data = request.middleware_data
+        return jsonify(middleware_data), 200 if middleware_data["status"] == "success" else 500
+    @app.route('/machines', methods=['POST'], endpoint='add_machines_middleware_endpoint')
+    @add_machines
+    def add_machines_middleware():
         middleware_data = request.middleware_data
         return jsonify(middleware_data), 200 if middleware_data["status"] == "success" else 500
     return app
--- backend/middleware.py
+++ backend/middleware.py
@@ -42,10 +42,13 @@ def get_user_settings(func):
     return wrapper
 def get_machines(func):
     def wrapper(*args, **kwargs):
-        type = request.args.get("type")
-        if not type:
+        machine_type = request.args.get("type")
+        if not machine_type:
             return {"status": "error", "message": "Missing machine type in request"}, 400
         # Make sure the enter a type that actually exists
+        if machine_type != "Official" or "User":
+            return {"status": "error", "message": "machine_type needs to be Official or User"}, 400
+
 
         try:
             # Query supabase for machines
@@ -70,3 +73,49 @@ def get_machines(func):
             request.middleware_data = {"status": "error", "message": str(e)}
         return func(*args, **kwargs)
     return wrapper
+def add_machines(func):
+    def wrapper(*args, **kwargs):
+        name = request.args.get("name")
+        machine_type = request.args.get("type")
+        brand = request.args.get("brand")
+        # TODO: Add Error handlers for the query parameters 
+        if not name:
+            return {"status": "error", "message": "Missing machine name in request"}, 400
+        if not machine_type:
+            return {"status": "error", "message": "Missing machine type in request"}, 400
+        if not brand:
+            return {"status": "error", "message": "Missing machine brand in request"}, 400
+        try:
+            data = supabase.table("machines").insert({"name": name, "type": machine_type, "brand": brand}).execute()
+            assert len(data.data) > 0
+            request.middleware_data = {"status": "success", "message": "successfully added machine to db"}
+        except Exception as e:
+            request.middleware_data = {"status": "error", "message": str(e)}
+        return func(*args, **kwargs)
+    return wrapper
+def add_machine_settings(func):
+    def wrapper(*args, **kwargs):
+        machine_id = request.args.get("machine_id")
+        user_id = request.args.get("user_id")
+        settings = request.get_json()
+        # TODO: Add Error handlers for the query parameters 
+        if not user_id:
+            return {"status": "error", "message": "Missing user_id in request"}, 400
+        if not settings:
+            return {"status": "error", "message": "Missing settings in request"}, 400
+        machine_settings = {
+            "machine_id": machine_id,
+            "settings": settings,
+            "user_id": user_id
+        }
+        try:
+            data = supabase.table("settings").insert(machine_settings).execute()
+            assert len(data.data) > 0
+            request.middleware_data = {"status": "success", "message": "successfully added machine setting to db"}
+        except Exception as e:
+            request.middleware_data = {"status": "error", "message": str(e)}
+        return func(*args, **kwargs)
+    return wrapper
+
+
+

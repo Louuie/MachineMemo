a2e09ef215b9ee9868122cc9c9cf9fe7c77789d2
Louie <eliasdandouch@gmail.com>
2025-01-10 12:56:52 -0800

    test

--- backend/middleware.py
+++ backend/middleware.py
@@ -18,7 +18,7 @@ def get_user_settings(func):
 
         try:
             # Query Supabase for settings
-            response = supabase.table("settings").select("*").eq("machine_id").execute()
+            response = supabase.table("settings").select("*").eq("machine_id", machine_id).execute()
             data = response.data  # Extract data
 
             # Format the data

55cb6b2d5c3b412c8052c68266f4d3be056c23c8
Eric <ezkeed125@gmail.com>
2025-02-12 14:38:59 -0800

    removed extra / in address

--- backend/middleware.py
+++ backend/middleware.py
@@ -9,7 +9,7 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY, options=ClientOptions(flow_type="pkce"))
 # No need for environment variable for this
-baseURl = "https://machinememo-5791cb7039d5.herokuapp.com/"
+baseURl = "https://machinememo-5791cb7039d5.herokuapp.com"
 
 def get_user_settings(func):
     @wraps(func)
